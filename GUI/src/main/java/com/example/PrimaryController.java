fb7f7daa5f5b2313122917d19effaea70fc11f36
CaseyBramlett <casey.bramlett243@gmail.com>
2023-02-27 16:03:26 -0600

    populate drop for locations

--- GUI/src/main/java/com/example/PrimaryController.java
+++ GUI/src/main/java/com/example/PrimaryController.java
@@ -1,12 +1,45 @@
 package com.example;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 
-public class PrimaryController {
+
+public class PrimaryController implements Initializable{
+    
+
+    //reservation details
+    @FXML
+    private ChoiceBox<String> locationBox;
+    private String[] Hotels = {"Hotel England","Hotel Sweden","Hotel France","Hotel Italy","Hotel Japan","Hotel Switzerland","Hotel Alabama","Hotel Arizona","Hotel Arkansas",
+                            "Hotel California","Hotel Colorado","Hotel Connecticut","Hotel Delaware","Hotel Florida","Hotel Georgia","Hotel Hawaii","Hotel Idaho","Hotel Illinois",
+                        "Hotel Indiana","Hotel Iowa","Hotel Kansas","Hotel Kentucky","Hotel Louisiana","Hotel Maine","Hotel Maryland","Hotel Massachusetts","Hotel Michigan",
+                    "Hotel Minnesota","Hotel Mississippi","Hotel Missouri","Hotel Montana","Hotel Nebraska","Hotel Nevada","Hotel New Hampshire","Hotel New Jersey",
+                "Hotel New Mexico","Hotel New York","Hotel North Carolina","Hotel North Dakota","Hotel Ohio","Hotel Oklahoma","Hotel Oregon","Hotel Pennsylvania",
+            "Hotel Rhode Island","Hotel South Carolina","Hotel South Dakota","Hotel Tennessee","Hotel Texas","Hotel Utah","Hotel Vermont","Hotel Virginia","Hotel Washington",
+        "Hotel West Virginia","Hotel Wisconsin","Hotel Wyoming"};
+    /*private ChoiceBox<String> roomBox;
+    private String[] Rooms={"1","2","3","4","5"};*/
+    private ChoiceBox<String> test;
+    private String testwords[] = {"1","2","3"};
+
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        loadData();
+        
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    private void loadData(){
+        //test.getItems().addAll(testwords);
+        locationBox.getItems().addAll(Hotels);
     }
 }
