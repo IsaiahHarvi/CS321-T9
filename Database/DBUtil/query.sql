433c2295f64ecbf1833806445c895d3a2475cc2f
Isaiah Harville <harvillerisaiah@gmail.com>
2023-04-07 15:57:17 -0500

    Update query.sql

--- Database/DBUtil/query.sql
+++ Database/DBUtil/query.sql
@@ -1,18 +1,24 @@
 --Query program to test SqliteJDBC.java
-
 /*
-SELECT *
-FROM Room
-JOIN Hotel
-ON Room.hotel_No = Hotel.hotel_No
 
-SELECT room_No, size, price, name, city
-FROM Room
-JOIN Hotel
-ON Room.hotel_No = Hotel.hotel_No 
-WHERE Hotel.city = 'Denver';
-*/
-
+--SELECT SPECIFIC GUEST BASED ON NAME
 SELECT * 
 FROM Guest
 WHERE Guest.first_name = 'John' AND Guest.last_name = 'Doe'
+
+
+--REMOVE GUEST AND BOOKING BY FIRST AND LAST NAME
+DELETE FROM Booking
+WHERE guest_No IN (
+    SELECT guest_No FROM Guest
+    WHERE first_name = 'John' AND last_name = 'Doe'
+);
+
+DELETE FROM Guest
+WHERE first_name = 'John' AND last_name = 'Doe';
+
+
+--GET LAST GUEST ADDED NUMBER
+SELECT MAX(guest_No) as largestGuestNumber FROM Guest
+
+*/
